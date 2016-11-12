48f5c4a0e02bfab9d419e10d330cf3c51418abaf
Manish Karn <m.karn2011@gmail.com>
2016-11-12 11:49:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/ui/Manish.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Manish.jpg'));
 });
 
+app.get('/ui/images/bgimg1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bgimg1.jpg'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
